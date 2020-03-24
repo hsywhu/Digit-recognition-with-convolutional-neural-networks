54091f13b6ce4668ed4c716a9930f55f9e04c03f
songyih <songyih1996@gmail.com>
2020-03-24 12:25:05 -0700

    pooling_layer_forward

--- matlab/pooling_layer_forward.m
+++ matlab/pooling_layer_forward.m
@@ -19,6 +19,26 @@ function [output] = pooling_layer_forward(input, layer)
 
     % Replace the following line with your implementation.
     output.data = zeros([h_out, w_out, c, batch_size]);
-
+    for batch_idx = 1:batch_size
+        % get the data for the current image in the batch
+        current_input = input.data(:, batch_idx);
+        % reshape the input image
+        current_input = reshape(current_input, [h_in, w_in, c]);
+        % pad the current image
+        current_input = padarray(current_input, [pad pad]);
+        % loop over the image to do the max pooling
+        for r_idx = 1:h_out
+            for c_idx = 1:w_out
+                for channel_idx = 1:c
+                    % compute the area for doing the pooling at the location
+                    pooling_area = current_input(1+(r_idx-1)*stride:k+(r_idx-1)*stride, 1+(c_idx-1)*stride:k+(c_idx-1)*stride, channel);
+                    % do max pooling on the area
+                    output.data(r_idx, c_idx, channel_idx, batch_idx) = max(pooling_area, [], 'all');
+                end
+            end
+        end
+    end
+    % reshape back the output
+    output.data = reshape(output.data, [h_out * w_out * c], batch_size);
 end
 
