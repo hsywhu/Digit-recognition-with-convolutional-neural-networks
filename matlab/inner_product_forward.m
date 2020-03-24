8e7f835f109717338aeb8c667c049d57dce6dec3
songyih <songyih1996@gmail.com>
2020-03-24 11:57:23 -0700

    inner_product_forward

--- matlab/inner_product_forward.m
+++ matlab/inner_product_forward.m
@@ -5,7 +5,16 @@ k = size(input.data, 2); % batch size
 n = size(param.w, 2);
 
 % Replace the following line with your implementation.
-output.data = zeros([n, k]);
-
+% the output of inner_product_forward should contain height, width,
+% channel, batch_size and data
+output.height = n;
+output.width = 1;
+output.channel = 1;
+output.batch_size = k;
+% data of output is given by: w * x + b
+w = transpose(param.w);
+x = input.data;
+b = transpose(param.b);
+output.data = w * x + b;
 
 end
